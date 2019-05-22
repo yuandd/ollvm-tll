cee6f8accfb1e81c64d67ebc5df5a1c929e8c61e
yuandd <yuandd_12@163.com>
2019-05-22 14:51:01 +0800

    处理部分字符串不能混淆的问题

--- include/llvm/Transforms/Obfuscation/StringObfuscation.h
+++ include/llvm/Transforms/Obfuscation/StringObfuscation.h
@@ -1,6 +1,22 @@
+#ifndef _STRINGOBFUSCATION_H_
+#define _STRINGOBFUSCATION_H_
+
 // LLVM include
 #include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/Transforms/IPO.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/CryptoUtils.h"
+
+// Namespace
+using namespace llvm;
+using namespace std;
 
 namespace llvm {
       Pass* createStringObfuscation(bool flag);
 }
+
+#endif
